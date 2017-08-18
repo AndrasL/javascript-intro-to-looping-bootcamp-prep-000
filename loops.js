675b7827f98ea1da58a5e45526d2b17b452311eb
Andras Lenart <andraslenart0@gmail.com>
2017-08-18 15:08:27 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,36 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    }
+    else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  let countdown = n
+  while (countdown > 0) {
+    console.log(--countdown)
+  }
+  if (countdown === 0) {
+    return 'done'
+  }
+}
+
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.splice(0, array.length-1)
+
+  } while (array.length > 0 && maybeTrue()) {
+    array.splice(0, array.length-1)
+  }
+  return array
+}
